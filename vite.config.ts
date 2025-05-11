7894a1f1f5b905419cccb1937f29bcf760778a18
Fahad <mofahadkp@proton.me>
2025-05-11 07:35:07 +0530

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "./", // This makes asset URLs relative instead of absolute
-
+  preview: {
+    port: 4173,
+    host: true, // bind to all network interfaces
+    strictPort: true,
+    allowedHosts: ["ranger.hackclub.app", "localhost"],
+  },
   plugins: [react(), tailwindcss()],
   assetsInclude: ["**/*.md"],
   build: {
